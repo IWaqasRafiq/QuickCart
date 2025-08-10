ac2a10709b494537332e1fbf527c30a6b78ca436
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-08-10 08:42:28 -0700

    set ID validation

--- config/inngest.js
+++ config/inngest.js
@@ -16,7 +16,7 @@ export const syncUserCreation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
-      id: id,
+      _id: id,
       name: first_name + "" + last_name,
       email: email_addresses[0].email_address,
       imageUrl: image_url,
@@ -37,7 +37,7 @@ export const syncUserUpdate = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
-      id: id,
+      _id: id,
       name: first_name + "" + last_name,
       email: email_addresses[0].email_address,
       imageUrl: image_url,
