ae1dfa2000efbc292c046013cac1506fd2247f70
Muhammad Waqas <119754074+IWaqasRafiq@users.noreply.github.com>
2025-08-10 05:17:29 -0700

    initialize inngest for userData storing

---
+++ config/inngest.js
@@ -0,0 +1,65 @@
+import { Inngest } from "inngest";
+import connectToDB from "./db";
+import User from "@/models/User";
+
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "quikcart" });
+
+export const syncUserCreation = inngest.createFunction(
+  {
+    id: "sync-user-from-clerk",
+  },
+  {
+    event: "clerk/user.created",
+  },
+  async (event) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      id: id,
+      name: first_name + "" + last_name,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await connectToDB();
+    await User.create(userData);
+  }
+);
+
+
+
+export const syncUserUpdate = inngest.createFunction(
+  {
+    id: "update-user-from-clerk",
+  },
+  {
+    event: "clerk/user.updated",
+  },
+  async (event) => {
+    const { id, first_name, last_name, email_addresses, image_url } =
+      event.data;
+    const userData = {
+      id: id,
+      name: first_name + "" + last_name,
+      email: email_addresses[0].email_address,
+      imageUrl: image_url,
+    };
+    await connectToDB();
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
+
+export const syncUserDeletion = inngest.createFunction(
+  {
+    id: "delete-user-from-clerk",
+  },
+  {
+    event: "clerk/user.deleted",
+  },
+  async (event) => {
+    const { id } = event.data;
+
+    await connectToDB();
+    await User.findByIdAndDelete(id);
+  }
+);
